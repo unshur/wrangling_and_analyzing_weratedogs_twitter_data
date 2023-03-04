1127156c7e94c876a79b8c8ae80dcdd326996c70
Ahmed Unshur <unshuramh@gmail.com>
2023-03-04 15:31:29 +0200

    Add `libraries.py` to repository

---
+++ libraries.py
@@ -0,0 +1,19 @@
+# Import required packages.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+import requests
+import tweepy
+import json
+
+
+# Enable inline plotting.
+%matplotlib inline
+
+# Set seaborn's default theme.
+sb.set_theme()
+
+# Ignore warnings.
+import warnings
+warnings.filterwarnings('ignore')
